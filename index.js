56f0ce339ec454176c1ce27b647c3a7463e3ce45
srindhupocharam2608@gmail.com <srindhupocharam2608@gmail.com>
2024-12-24 11:22:22 +0530

    js script added for verification

---
+++ index.js
@@ -0,0 +1,26 @@
+// creating variables
+
+const search = document.querySelector("#search");
+const searchField = document.querySelector("#city");
+const searchbtn = document.querySelector("#searchbtn");
+const searchbar = document.querySelector("#form");
+const content = document.querySelector("#content");
+const temp = document.querySelector("#temp");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind");
+const location1 = document.querySelector("#location");
+const day1 = document.querySelector("#d1");
+const day2 = document.querySelector("#d2");
+const day3 = document.querySelector("#d3");
+const day4 = document.querySelector("#d4");
+const day5 = document.querySelector("#d5");
+const day6 = document.querySelector("#d6");
+const day7 = document.querySelector("#d7");
+const body = document.querySelector("#mainCard");
+const condition = document.querySelector("#condition");
+const recentCitiesDropdown = document.querySelector("#suggestionsContainer");
+const showForecast = document.querySelector("#forecast");
+const showForecastbtn = document.querySelector("#forecastbtn");
+const hideForecastbtn = document.querySelector("#hideforecastbtn");
+const mbody = document.querySelector("#body");
+const currentLocation = document.querySelector("#btnLocation");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html","./index.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+
+
+
+
+    },
   },
   plugins: [],
 }
